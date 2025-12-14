d84cd1d5d463a48d9a97e773f903b00be8094c0d
eduard <eduard.synohub@gmail.com>
2025-12-14 19:23:04 +0100

    lesson 4

--- lesson_4.py
+++ lesson_4.py
@@ -17,3 +17,49 @@ for number in list_of_numbers:
     list_of_double_numbers.append(number * 2)
 
 print(list_of_double_numbers)
+
+print("Task 4:")
+
+fruits = ['apple', 'banana', 'orange']
+for fruit in fruits:
+    print(fruit)
+
+print("Task 5:")
+
+tuple_1 = (1, 4, 23, 11, 43)
+tuple_2 = (4, 43, 22, 18)
+
+tuple_3 = tuple_1 + tuple_2
+print(tuple_3)
+
+print("Task 6:")
+
+sport_mans = {"name": "Michael", "surname": "Jordan", "age": 25, "team": "Chickago Bulls"}
+print(sport_mans)
+
+print("Task 7:")
+
+books = {"author": "Rowling", "year": 2000}
+print(books)
+book_update = {"author": "Jack", "year": 1994}
+books.update(book_update)
+print(books)
+
+print("Task 8:")
+
+capitals = {"Ukraine": "Kiev",
+            "USA": "Washington",
+            "Poland": "Warsaw",
+            "United Kingdom": "London"}
+print(*capitals)
+
+while True:
+    n = input("Enter name of country: ")
+    if n in capitals:
+        print(capitals[n])
+        break
+    else:
+        print("We don`t have that country")
+
+print("Task 9:")
+
